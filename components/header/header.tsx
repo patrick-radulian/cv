6dc4be56bb8b94a95df4d8c651feabd2cd2e91b3
Patrick Radulian <patrick.radulian@gmail.com>
2021-09-18 10:58:01 +0200

    Fine tune profile image

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -5,7 +5,9 @@ import utilStyles from "../../styles/utils.module.css";
 export default function Header() {
     return (
         <header className={styles.header}>
-            <Image priority src="/images/profile.jpg" className={styles.profileImage} height={196} width={196} alt="Patrick Radulian"/>
+            <div className={styles.imageContainer}>
+                <Image priority src="/images/profile.jpg" className={styles.profileImage} height={196} width={196} alt="Patrick Radulian"/>
+            </div>
 
             <div className={styles.name}>
                 <span className={`${utilStyles.colorSecondary} ${utilStyles.underline}`}>cv</span>
