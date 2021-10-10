2b6d70fb5c62b2971dfc16d20447fa59726ddf68
Patrick Radulian <patrick.radulian@gmail.com>
2021-10-10 19:00:54 +0200

    Rename Header_Entry back to HeaderEntry

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import styles from "./Header.module.css"
 import utilStyles from "../../styles/utils.module.css";
-import HeaderEntry from "../Header_Entry/HeaderEntry";
+import HeaderEntry from "../HeaderEntry/HeaderEntry";
 import React from "react";
 
 export default function Header() {
@@ -19,15 +19,15 @@ export default function Header() {
             </div>
 
             <div className={styles.headerEntries}>
-                {/* <HeaderEntry imageFileName="person.svg" imageAltText="Person" text="patrick-radulian.com" link="https://patrick-radulian.com"/> */}
-                {/* <HeaderEntry imageFileName="phone.svg" imageAltText="Phone" text="+43 699 170 51 561" link="tel:+43-699-170-51-561"/> */}
-                {/* <HeaderEntry imageFileName="envelope.svg" imageAltText="Envelope" text="patrick.radulian@gmail.com" link="mailto:patrick.radulian@gmail.com"/> */}
+                <HeaderEntry imageFileName="person.svg" imageAltText="Person" text="patrick-radulian.com" link="https://patrick-radulian.com"/>
+                <HeaderEntry imageFileName="phone.svg" imageAltText="Phone" text="+43 699 170 51 561" link="tel:+43-699-170-51-561"/>
+                <HeaderEntry imageFileName="envelope.svg" imageAltText="Envelope" text="patrick.radulian@gmail.com" link="mailto:patrick.radulian@gmail.com"/>
             </div>
 
             <div className={styles.headerEntries}>
-                {/* <HeaderEntry imageFileName="xing.svg" imageAltText="Xing Logo" text="patrick radulian" link="https://www.xing.com/profile/Patrick_Radulian/cv"/> */}
-                {/* <HeaderEntry imageFileName="location.svg" imageAltText="Map Location Pin" text="vienna" link="https://goo.gl/maps/ArAvLCbSomd8XuNM9"/> */}
-                {/* <HeaderEntry imageFileName="linkedin.svg" imageAltText="LinkedIn Logo" text="in/patrickradulian" link="https://www.linkedin.com/in/patrickradulian/"/> */}
+                <HeaderEntry imageFileName="xing.svg" imageAltText="Xing Logo" text="patrick radulian" link="https://www.xing.com/profile/Patrick_Radulian/cv"/>
+                <HeaderEntry imageFileName="location.svg" imageAltText="Map Location Pin" text="vienna" link="https://goo.gl/maps/ArAvLCbSomd8XuNM9"/>
+                <HeaderEntry imageFileName="linkedin.svg" imageAltText="LinkedIn Logo" text="in/patrickradulian" link="https://www.linkedin.com/in/patrickradulian/"/>
             </div>
         </header>
     )
