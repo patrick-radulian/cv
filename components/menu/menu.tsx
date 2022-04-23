bd2ff68e9bd02789b183221f81bdf010932dc0fb
Patrick Radulian <patrick.radulian@gmail.com>
2022-04-23 13:31:19 +0200

    Adjust menu and signature

--- components/menu/menu.tsx
+++ components/menu/menu.tsx
@@ -3,12 +3,13 @@ import Link from "next/link"
 import Image from "next/image"
 import styles from "./menu.module.css"
 import SignatureSVG from "../../public/images/Signature.svg"
+import SignaturePNG from "../../public/images/Signature.png"
 
 const Menu: React.FC<{}> = () => {
     return (
         <div className={styles["menu-container"]}>
             <div className={styles.menu}>
-                <Image src={SignatureSVG} className={styles.signature}/>
+                <Image src={SignaturePNG} objectFit="contain" className={styles.signature}/>
 
                 <div className={`${styles.cloud} ${styles.cloud1}`}/>
                 <div className={`${styles.cloud} ${styles.cloud2}`}/>
