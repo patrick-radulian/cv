b612c1241b137d7533d83ea8271846ae497d017f
Patrick Radulian <patrick.radulian@gmail.com>
2022-04-26 10:00:37 +0200

    Fine tune skills section

--- components/skills/skills.tsx
+++ components/skills/skills.tsx
@@ -17,33 +17,73 @@ import UNITY from "public/images/skills/Unity.svg"
 import UNREAL from "public/images/skills/UE.svg"
 import CS from "public/images/skills/CS.svg"
 import NODE from "public/images/skills/Node.svg"
+import PRESENTING from "public/images/skills/Presenting.svg"
+import PROTOTYPING from "public/images/skills/Prototyping.svg"
+import DT from "public/images/skills/DesignThinking.svg"
+import INTERVIEWING from "public/images/skills/Interviewing.svg"
+import STORYTELLING from "public/images/skills/StoryTelling.svg"
+import USERRESEARCH from "public/images/skills/UserResearch.svg"
+import FACILITATING from "public/images/skills/Facilitating.svg"
 
 const Skills: React.FC<{}> = () => {
     return (
         <div className={styles.skills}>
             <CVSection icon={SkillsIcon} label="Skills">
                 <div className={styles["skill-grid"]}>
-                    <div className={`${styles["skill-group"]} ${styles["group-a"]}`}>
-                        <Image src={HTML} width={80} height={80}/>
-                        <Image src={CSS} width={80} height={80}/>
-                        <Image src={JS} width={80} height={80}/>
-                        <Image src={REACT} width={80} height={80}/>
-                        <Image src={TS} width={80} height={80}/>
+                    <div className={styles["skill-group"]}>
+                        <a href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank"><Image src={HTML} width={80} height={80} className={styles.skill}/></a>
+                        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank"><Image src={CSS} width={80} height={80} className={styles.skill}/></a>
+                        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"><Image src={JS} width={80} height={80} className={styles.skill}/></a>
+                        <a href="https://reactjs.org/" target="_blank"><Image src={REACT} width={80} height={80} className={styles.skill}/></a>
+                        <a href="https://www.typescriptlang.org/" target="_blank"><Image src={TS} width={80} height={80} className={styles.skill}/></a>
                     </div>
-                    <div className={`${styles["skill-group"]} ${styles["group-b"]}`}>
-                        <Image src={UXPIN} width={60} height={60}/>
-                        <Image src={ADOBECS} width={60} height={60}/>
-                        <Image src={PROTOIO} width={60} height={60}/>
+                    <div className={styles["skill-group"]}>
+                        <a href="https://www.uxpin.com/" target="_blank"><Image src={UXPIN} width={70} height={70} className={styles.skill}/></a>
+                        <a href="https://www.adobe.com/creativecloud.html" target="_blank"><Image src={ADOBECS} width={70} height={70} className={styles.skill}/></a>
+                        <a href="https://proto.io/" target="_blank"><Image src={PROTOIO} width={70} height={70} className={styles.skill}/></a>
                     </div>
-                    <div className={`${styles["skill-group"]} ${styles["group-c"]}`}>
-                        <Image src={WEBPACK} width={45} height={45}/>
-                        <Image src={GIT} width={45} height={45}/>
-                        <Image src={UNITY} width={45} height={45}/>
-                        <Image src={UNREAL} width={45} height={45}/>
+                    <div className={styles["skill-group"]}>
+                        <a href="https://webpack.js.org/" target="_blank"><Image src={WEBPACK} width={60} height={60} className={styles.skill}/></a>
+                        <a href="https://git-scm.com/" target="_blank"><Image src={GIT} width={60} height={60} className={styles.skill}/></a>
+                        <a href="https://unity.com/" target="_blank"><Image src={UNITY} width={60} height={60} className={styles.skill}/></a>
+                        <a href="https://www.unrealengine.com/" target="_blank"><Image src={UNREAL} width={60} height={60} className={styles.skill}/></a>
                     </div>
-                    <div className={`${styles["skill-group"]} ${styles["group-d"]}`}>
-                        <Image src={CS} width={45} height={45}/>
-                        <Image src={NODE} width={45} height={45}/>
+                    <div className={styles["skill-group"]}>
+                        <a href="https://docs.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/" target="_blank"><Image src={CS} width={50} height={50} className={styles.skill}/></a>
+                        <a href="https://nodejs.org/" target="_blank"><Image src={NODE} width={50} height={50} className={styles.skill}/></a>
+                    </div>
+                    <div className={styles["skill-group"]}>
+                        <div className={`${styles["skill-container"]} ${styles["skill-container-a"]}`}>
+                            <Image src={PRESENTING} width={60} height={60} className={styles.skill}/>
+                            <strong className={styles["skill-name"]}>Presenting</strong>
+                        </div>
+                        <div className={`${styles["skill-container"]} ${styles["skill-container-a"]}`}>
+                        <Image src={PROTOTYPING} width={60} height={60} className={styles.skill}/>
+                            <strong className={styles["skill-name"]}>Prototyping</strong>
+                        </div>
+                        <div className={`${styles["skill-container"]} ${styles["skill-container-a"]}`}>
+                        <Image src={DT} width={60} height={60} className={styles.skill}/>
+                            <strong className={styles["skill-name"]}>Design Thinking</strong>
+                        </div>
+                    </div>
+
+                    <div className={styles["skill-group"]}>
+                        <div className={`${styles["skill-container"]} ${styles["skill-container-b"]}`}>
+                            <Image src={INTERVIEWING} width={50} height={50} className={styles.skill}/>
+                            <strong className={styles["skill-name"]}>Interviewing</strong>
+                        </div>
+                        <div className={`${styles["skill-container"]} ${styles["skill-container-b"]}`}>
+                            <Image src={STORYTELLING} width={50} height={50} className={styles.skill}/>
+                            <strong className={styles["skill-name"]}>Story Telling</strong>
+                        </div>
+                        <div className={`${styles["skill-container"]} ${styles["skill-container-b"]}`}>
+                            <Image src={USERRESEARCH} width={50} height={50} className={styles.skill}/>
+                            <strong className={styles["skill-name"]}>User Research</strong>
+                        </div>
+                        <div className={`${styles["skill-container"]} ${styles["skill-container-b"]}`}>
+                            <Image src={FACILITATING} width={50} height={50} className={styles.skill}/>
+                            <strong className={styles["skill-name"]}>Facilitating</strong>
+                        </div>
                     </div>
                 </div>
             </CVSection>
