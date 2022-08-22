9003a24fd2c7684db22fa0915fa82643fa18ef33
Patrick Radulian <patrick.radulian@gmail.com>
2022-08-22 20:23:43 +0200

    Remove google fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import Main from 'components/main/main'
-import Head from 'next/head'
-import React from 'react'
-import Arrow from '../components/arrow/arrow'
-import Background from '../components/background/background'
-import Clouds from '../components/clouds/clouds'
+import Main from "components/main/main";
+import Head from "next/head";
+import React from "react";
+import Arrow from "../components/arrow/arrow";
+import Background from "../components/background/background";
+import Clouds from "../components/clouds/clouds";
 
-import StartScreen from '../components/start-screen/start-screen'
+import StartScreen from "../components/start-screen/start-screen";
 
 export default function Home() {
     return (
@@ -13,23 +13,19 @@ export default function Home() {
             <Head>
                 <title>Patrick Radulian</title>
                 <link rel="icon" href="/favicon.ico" />
-                <meta name="description" content="Patrick Radulian's CV and Portfolio"/>
-                <meta name="og:title" content="Patrick Radulian"/>
-
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com"/>
-                <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100;0,300;0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
+                <meta name="description" content="Patrick Radulian's CV and Portfolio" />
+                <meta name="og:title" content="Patrick Radulian" />
             </Head>
 
-            <Background/>
+            <Background />
 
-            <StartScreen/>
+            <StartScreen />
 
-            <Clouds/>
+            <Clouds />
 
-            <Arrow/>
+            <Arrow />
 
-            <Main/>
+            <Main />
         </div>
-    )
+    );
 }
