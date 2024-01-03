7a2854b1d314f1966ccec2ec3c1400277f0e4277
Patrick Radulian <patrick.radulian@gmail.com>
2024-01-03 17:19:08 +0100

    Add react-router-dom package

---
+++ src/pages/new-portfolio/new-portfolio.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewPortfolio: React.FC = () => {
+    return (
+        <div>
+            <h1>New Portfolio</h1>
+        </div>
+    );
+};
+
+export default NewPortfolio;
