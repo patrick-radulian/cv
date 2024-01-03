dc26597a9e4000221bbc95ece62667eac6268561
Patrick Radulian <patrick.radulian@gmail.com>
2024-01-03 19:00:28 +0100

    Remove portfolio page and related files

--- src/pages/root/root.tsx
+++
@@ -1,8 +0,0 @@
-import styles from "./root.module.css";
-import React, { PropsWithChildren } from "react";
-
-const Root: React.FC<PropsWithChildren> = ({ children }) => {
-    return <main className={styles.main}>{children}</main>;
-};
-
-export default Root;
